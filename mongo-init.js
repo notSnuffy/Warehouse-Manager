37eda9b7fa18674606f20304b709741ba0ac3508
notSnuffy <96662342+notSnuffy@users.noreply.github.com>
2024-04-20 06:12:15 +0000

    Add mongo services

---
+++ mongo-init.js
@@ -0,0 +1,7 @@
+db.auth("root", "password");
+db = db.getSiblingDB("WarehouseManager");
+db.createUser({
+  user: "WarehouseManagerUser",
+  pwd: "password",
+  roles: [{ role: "readWrite", db: "WarehouseManager" }],
+});
