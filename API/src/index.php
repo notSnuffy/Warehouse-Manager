38a207d038cb89f0a0da74f9615600d39a1234ad
notSnuffy <96662342+notSnuffy@users.noreply.github.com>
2024-04-21 06:23:42 +0000

    Add slim framework

--- API/src/index.php
+++ API/src/index.php
@@ -3,20 +3,63 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 use MongoDB\Client;
 
-$uri = "mongodb://WarehouseManagerUser:password@mongodb:27017/WarehouseManager";
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Server\RequestHandlerInterface;
+use Slim\Factory\AppFactory;
+use Slim\Routing\RouteCollectorProxy;
+use Slim\Routing\RouteContext;
 
-$client = new Client($uri);
+$app = AppFactory::create();
 
-try {
-    $database = $client->selectDatabase("WarehouseManager");
-    $collection = $database->selectCollection("test");
-    $collection->insertOne(["name" => "test"]);
-} catch (Exception $e) {
-    echo "Error: " . $e->getMessage();
-}
+$app->addBodyParsingMiddleware();
 
-echo "Hello world!!";
+// This middleware will append the response header Access-Control-Allow-Methods with all allowed methods
+$app->add(function (Request $request, RequestHandlerInterface $handler): Response {
+    $routeContext = RouteContext::fromRequest($request);
+    $routingResults = $routeContext->getRoutingResults();
+    $methods = $routingResults->getAllowedMethods();
+    $requestHeaders = $request->getHeaderLine('Access-Control-Request-Headers');
 
-phpinfo();
+    $response = $handler->handle($request);
+
+    $response = $response->withHeader('Access-Control-Allow-Origin', '*');
+    $response = $response->withHeader('Access-Control-Allow-Methods', implode(',', $methods));
+    $response = $response->withHeader('Access-Control-Allow-Headers', $requestHeaders);
+
+    // Optional: Allow Ajax CORS requests with Authorization header
+    // $response = $response->withHeader('Access-Control-Allow-Credentials', 'true');
+
+    return $response;
+});
+
+// The RoutingMiddleware should be added after our CORS middleware so routing is performed first
+$app->addRoutingMiddleware();
+
+$app->get('/', function (Request $request, Response $response, $args) {
+    $newResponse = $response->withHeader('Content-type', 'application/json');
+    $newResponse->getBody()->write(json_encode(['message' => 'Hello World!']));
+    return $newResponse;
+});
+
+$app->options('/{routes:.+}', function (Request $request, Response $response, $args) {
+    return $response;
+});
+
+$app->run();
+
+// $uri = "mongodb://WarehouseManagerUser:password@mongodb:27017/WarehouseManager";
+
+// $client = new Client($uri);
+
+// try {
+//     $database = $client->selectDatabase("WarehouseManager");
+//     $collection = $database->selectCollection("test");
+//     $collection->insertOne(["name" => "test"]);
+// } catch (Exception $e) {
+//     echo "Error: " . $e->getMessage();
+// }
+
+// phpinfo();
 ?>
 
