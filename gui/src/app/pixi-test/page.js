d73a8317b93f6de2253a4fe68917adb216737e0e
notSnuffy <96662342+notSnuffy@users.noreply.github.com>
2024-04-22 23:50:01 +0000

    Change shapes to be within a container

--- gui/src/app/pixi-test/page.js
+++ gui/src/app/pixi-test/page.js
@@ -1,11 +1,27 @@
 "use client";
+import React from "react";
 import "@pixi/events";
-import { Stage, Graphics, useApp } from "@pixi/react";
+import { Container, Stage, Graphics, useApp } from "@pixi/react";
 import { useCallback } from "react";
 
-function Rectangle() {
-  const app = useApp();
+function Rectangle({ x, y, width, height, onDragStart }) {
+  const draw = useCallback((g) => {
+    g.clear();
+    g.beginFill(0xff0000);
+    g.drawRect(0, 0, width, height);
+    g.endFill();
+    g.x = x;
+    g.y = y;
+    g.pivot.set(width / 2, height / 2);
+    g.eventMode = "static";
+    g.on("pointerdown", onDragStart);
+  }, []);
 
+  return <Graphics draw={draw} />;
+}
+
+function ShapeContainer({ x, y, children }) {
+  const app = useApp();
   let dragTarget = null;
   const onDragMove = (event) => {
     if (dragTarget) {
@@ -28,24 +44,20 @@ function Rectangle() {
     }
   };
 
-  const draw = useCallback(
-    (g) => {
-      g.clear();
-      g.beginFill(0xff0000);
-      g.drawRect(0, 0, 100, 100);
-      g.endFill();
-      g.x = 100;
-      g.y = 100;
-      g.pivot.set(50, 50);
-      g.eventMode = "static";
-      g.on("pointerdown", onDragStart);
-      app.stage.on("pointerup", onDragEnd);
-      app.stage.on("pointerupoutside", onDragEnd);
-    },
-    [app.stage],
-  );
+  app.stage.on("pointerup", onDragEnd);
+  app.stage.on("pointerupoutside", onDragEnd);
 
-  return <Graphics draw={draw} />;
+  console.log(children);
+
+  const childrenWithOnDragStart = React.Children.map(children, (child) => {
+    return React.cloneElement(child, { onDragStart });
+  });
+
+  return (
+    <Container x={x} y={y} eventMode={"static"}>
+      {childrenWithOnDragStart}
+    </Container>
+  );
 }
 
 function PixiComponent() {
@@ -53,10 +65,13 @@ function PixiComponent() {
     app.stage.eventMode = "static";
     app.stage.hitArea = app.screen;
   };
+
   return (
     <Stage x={800} y={600} onMount={onMount}>
-      <Rectangle />
-      <Rectangle />
+      <ShapeContainer x={100} y={100}>
+        <Rectangle x={0} y={0} width={100} height={100} />
+        <Rectangle x={200} y={200} width={100} height={100} />
+      </ShapeContainer>
     </Stage>
   );
 }
