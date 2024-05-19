7d3ad4416149e7f880284b4ba4d2701118aa67c5
notSnuffy <96662342+notSnuffy@users.noreply.github.com>
2024-05-19 23:16:23 +0000

    Stack changes

--- gui/src/app/PrimeProvider.js
+++
@@ -1,8 +0,0 @@
-"use client";
-import { PrimeReactProvider } from "primereact/api";
-import "./globals.css";
-import "primereact/resources/themes/md-dark-indigo/theme.css";
-
-export default function PrimeProvider({ children }) {
-  return <PrimeReactProvider>{children}</PrimeReactProvider>;
-}
--- gui/tailwind.config.js
+++ gui/tailwind.config.js
@@ -1,11 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/primereact/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./src/**/*.{js,html}"],
   darkMode: "class",
   theme: {
     extend: {},
