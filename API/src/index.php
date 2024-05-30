a23a461c05ac28541b14c4615da78a5bd9826a73
notSnuffy <96662342+notSnuffy@users.noreply.github.com>
2024-05-30 05:58:19 +0000

    Add linting and formatting for API

--- API/src/index.php
+++ API/src/index.php
@@ -1,4 +1,5 @@
 <?php
+
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use MongoDB\Client;
@@ -7,7 +8,6 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Server\RequestHandlerInterface;
 use Slim\Factory\AppFactory;
-use Slim\Routing\RouteCollectorProxy;
 use Slim\Routing\RouteContext;
 
 $app = AppFactory::create();
@@ -69,5 +69,3 @@ $app->run();
 // }
 
 // phpinfo();
-?>
-
