7d1c55677d8dbcac0875cdaf713b5956a6850070
holublu2 <l.vrkyholub@gmail.com>
2025-07-25 12:12:43 +0200

    Change directory structure

--- mongo-init.js
+++
@@ -1,8 +0,0 @@
-db.auth("root", "password");
-db = db.getSiblingDB("WarehouseManager");
-db.createUser({
-  user: "WarehouseManagerUser",
-  pwd: "password",
-  roles: [{ role: "readWrite", db: "WarehouseManager" }],
-});
-db.createCollection("test");
