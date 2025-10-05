a0fa14bb11e32abf8a7b2ec1c0829231c1c8bd53
holublu2 <l.vrkyholub@gmail.com>
2025-10-05 03:17:45 +0200

    Fix zooming

--- services/gui/src/lib/ZoomManager.js
+++ services/gui/src/lib/ZoomManager.js
@@ -28,76 +28,47 @@ class ZoomManager {
    * @private
    */
   #zoomTowardsPointer(pointer, deltaY) {
-    //const camera = this.scene.cameras.main;
-
-    //const worldPointer = {
-    //  x: pointer.worldX,
-    //  y: pointer.worldY,
-    //};
-    //console.log("World pointer:", worldPointer);
-
-    //const zoomDirection = deltaY > 0 ? -1 : 1;
-    //let newZoom = camera.zoom + zoomDirection * this.#zoomStep;
-    //newZoom = Phaser.Math.Clamp(newZoom, 0.5, 4);
-    //console.log("New zoom:", newZoom);
-
-    //camera.setZoom(newZoom);
-
-    //const newWorldPointer = camera.getWorldPoint(
-    //  pointer.worldX,
-    //  pointer.worldY,
-    //);
-    //console.log("New world pointer:", newWorldPointer);
-
-    //console.log("Old scroll:", { x: camera.scrollX, y: camera.scrollY });
-    //camera.scrollX += worldPointer.x - newWorldPointer.x;
-    //camera.scrollY += worldPointer.y - newWorldPointer.y;
-    //console.log("New scroll:", { x: camera.scrollX, y: camera.scrollY });
-
     const camera = this.scene.cameras.main;
 
-    const worldPointer = {
-      x: pointer.worldX,
-      y: pointer.worldY,
-    };
-
-    console.log("Previous scroll:", { x: camera.scrollX, y: camera.scrollY });
-
-    const centerScroll = camera.getScroll(worldPointer.x, worldPointer.y);
-    console.log("Center scroll:", centerScroll);
-    const previousZoom = camera.zoom;
-    console.log("Previous zoom:", previousZoom);
+    const oldZoom = camera.zoom;
 
     const zoomDirection = deltaY > 0 ? -1 : 1;
     let newZoom = camera.zoom + zoomDirection * this.#zoomStep;
     newZoom = Phaser.Math.Clamp(newZoom, 0.5, 4);
+
+    const cameraPointerPosition = {
+      x: pointer.x,
+      y: pointer.y,
+    };
+
+    const cameraCenter = {
+      x: camera.centerX,
+      y: camera.centerY,
+    };
+
+    const pointerOffsetFromCenter = {
+      x: cameraPointerPosition.x - cameraCenter.x,
+      y: cameraPointerPosition.y - cameraCenter.y,
+    };
+
+    const pointerOffsetFromCenterRatio = {
+      x: pointerOffsetFromCenter.x / camera.width,
+      y: pointerOffsetFromCenter.y / camera.height,
+    };
+
+    const pixelDifferenceAfterZoom = {
+      width: camera.width / oldZoom - camera.width / newZoom,
+      height: camera.height / oldZoom - camera.height / newZoom,
+    };
+
+    const scrollAdjustment = {
+      x: pixelDifferenceAfterZoom.width * pointerOffsetFromCenterRatio.x,
+      y: pixelDifferenceAfterZoom.height * pointerOffsetFromCenterRatio.y,
+    };
+
     camera.setZoom(newZoom);
-    console.log("New zoom:", newZoom);
-
-    console.log("World pointer:", worldPointer);
-
-    if (previousZoom === newZoom) {
-      return;
-    }
-
-    const quadrantX = worldPointer.x >= centerScroll.x ? 1 : -1;
-    const quadrantY = worldPointer.y >= centerScroll.y ? 1 : -1;
-
-    const worldPointerOffset = {
-      x: worldPointer.x - (camera.width / 2) * quadrantX,
-      y: worldPointer.y - (camera.height / 2) * quadrantY,
-    };
-    console.log("World pointer offset:", worldPointerOffset);
-
-    const adjustedOffset = {
-      x: worldPointerOffset.x / newZoom,
-      y: worldPointerOffset.y / newZoom,
-    };
-    console.log("Adjusted offset:", adjustedOffset);
-
-    camera.scrollX = centerScroll.x - adjustedOffset.x;
-    camera.scrollY = centerScroll.y - adjustedOffset.y;
-    console.log("New scroll:", { x: camera.scrollX, y: camera.scrollY });
+    camera.scrollX += scrollAdjustment.x;
+    camera.scrollY += scrollAdjustment.y;
   }
 
   /**
